34ef0a883cc21fa28b32e4a56636a9e23474197f
Elena Shkurenko <shkurenko.elen@gmail.com>
2023-04-02 23:26:50 +0300

    2 задание 6 спринта. Работа модальных окон

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,35 @@ const aboutUser = document.querySelector('.profile__about-user');
 const buttonClose = popup.querySelector('.popup__close');
 const buttonSave = popup.querySelector('.popup__save');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+const gallery = document.querySelector('.gallery');
+const templateCard = document.querySelector('#template-card').content;
+
 buttonEdit.addEventListener('click', () => {
   popup.classList.add('popup_opened');
   inputUsername.value = username.textContent;
@@ -28,3 +57,16 @@ function handleFormSubmit(evt){
 }
 
 buttonSave.addEventListener('click', handleFormSubmit);
+
+initialCards.forEach((item) => {
+  const card = templateCard.querySelector('.gallery__item').cloneNode(true);
+  const cardImage = card.querySelector('.gallery__image');
+  const cardTitle = card.querySelector('.gallery__title');
+  cardImage.src= item.link;
+  cardImage.alt= item.name;
+  cardTitle.textContent = item.name;
+
+  gallery.append(card);
+});
+
+
